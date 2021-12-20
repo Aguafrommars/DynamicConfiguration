2370e100638cd7cbbe3b158073ba027561c2bbef
Olivier Lefebvre <olivier.lefebvre@live.com>
2021-12-20 09:47:18 +0100

    ci: run sonarqube on same repo only

--- build.ps1
+++ build.ps1
@@ -18,12 +18,18 @@ if ($prNumber) {
 elseif ($env:APPVEYOR_REPO_BRANCH) {
 	$prArgs = "-d:sonar.branch.name=$env:APPVEYOR_REPO_BRANCH"
 }
-Write-Host "dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=****** -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version"
-dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=$env:sonarqube -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version
 
-Write-Host "dotnet test -c Release --settings coverletArgs.runsettings --filter FullyQualifiedName!~MongoDb"
+if (-Not $env:APPVEYOR_PULL_REQUEST_NUMBER) {
+	Write-Host "dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=****** -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version"
+	dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=$env:sonarqube -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version
+} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME -eq $env:APPVEYOR_REPO_NAME) {
+	Write-Host "dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=****** -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version"
+	dotnet sonarscanner begin /k:Aguafrommars_DynamicConfiguration -o:aguacongas -d:sonar.host.url=https://sonarcloud.io -d:sonar.login=$env:sonarqube -d:sonar.coverageReportPaths=coverage\SonarQube.xml $prArgs -v:$env:Version
+}
 
-dotnet test -c Release --collect:"XPlat Code Coverage" --settings coverletArgs.runsettings --filter "FullyQualifiedName!~MongoDb" -v q
+Write-Host "dotnet test -c Release --settings coverletArgs.runsettings  -v q"
+
+dotnet test -c Release --collect:"XPlat Code Coverage" --settings coverletArgs.runsettings -v q
 
 if ($LASTEXITCODE -ne 0) {
 	$result = $LASTEXITCODE
@@ -39,6 +45,9 @@ Get-ChildItem -rec `
 Write-Host $merge
 ReportGenerator\tools\net5.0\ReportGenerator.exe "-reports:$merge" "-targetdir:coverage" "-reporttypes:SonarQube"
 	
-dotnet sonarscanner end -d:sonar.login=$env:sonarqube
-
+if (-Not $env:APPVEYOR_PULL_REQUEST_NUMBER) {
+	dotnet sonarscanner end -d:sonar.login=$env:sonarqube
+} elseif ($env:APPVEYOR_PULL_REQUEST_HEAD_REPO_NAME -eq $env:APPVEYOR_REPO_NAME) {
+	dotnet sonarscanner end -d:sonar.login=$env:sonarqube
+}
 exit $result
